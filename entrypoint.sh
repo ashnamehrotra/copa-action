93a52e8f8700f8e12a5a871e675f5f54e3e987a0
ashnamehrotra <ashnamehrotra@gmail.com>
2024-02-08 11:12:46 -0800

    debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,7 +32,7 @@ fi
 echo "final command: copa patch -i $image -r ./data/"$report" -t $patched_tag $buildkitd --timeout $timeout $output"
 copa_output=$(copa patch -i "$image" -r ./data/"$report" -t "$patched_tag" $buildkitd --timeout $timeout $output;)
 echo "COPA OUTPUT"
-echo "$copa_output
+echo "$copa_output"
 
 # run copa to patch image
 if copa patch -i "$image" -r ./data/"$report" -t "$patched_tag" $buildkitd --timeout $timeout $output;
