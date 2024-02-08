f10d768210bbbfd2f065980e3dc9a6e7f80afbf9
ashnamehrotra <ashnamehrotra@gmail.com>
2024-02-08 12:13:53 -0800

    remove docker buildx ls
    Signed-off-by: ashnamehrotra <ashnamehrotra@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,8 +30,6 @@ else
     buildkitd="--addr tcp://127.0.0.1:8888"
 fi
 
-docker buildx ls
-
 # run copa to patch image
 if copa patch -i "$image" -r ./data/"$report" -t "$patched_tag" $buildkitd --timeout $timeout $output;
 then
