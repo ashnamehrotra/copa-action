418eb77b87a884e1b2512c958ba13c837f1992d4
ashnamehrotra <ashnamehrotra@gmail.com>
2024-02-08 15:06:46 -0800

    clean up 3 - testing ctr d img store
    Signed-off-by: ashnamehrotra <ashnamehrotra@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ patched_tag=$3
 timeout=$4
 output_file=$5
 format=$6
-buildkitd_container=$7
+connection-format=$7
 
 
 # parse image into image name
@@ -22,8 +22,23 @@ else
     output="--format $format --output ./data/"$output_file""
 fi
 
+# check selected method of connection
+# through a running buildkit container
+if [connection-format == "buildkit-container"]; then
+     connection="--addr tcp://127.0.0.1:8888"
+# via buildx instance (default method - allows for patching private images)
+elif [connection-format == "buildx"]; then
+    docker buildx create --name=copa-action
+    docker buildx use --default copa-action
+    connection="--addr buildx://copa-action"
+# through a custom socket enabling containerd img store (allows for patching local and private images)
+else
+    connection=""
+fi
+
+
 # run copa to patch image
-if copa patch -i "$image" -r ./data/"$report" -t "$patched_tag" $buildkitd --timeout $timeout $output;
+if copa patch -i "$image" -r ./data/"$report" -t "$patched_tag" $connection --timeout $timeout $output;
 then
     patched_image="$image_no_tag:$patched_tag"
     echo "patched-image=$patched_image" >> "$GITHUB_OUTPUT"
